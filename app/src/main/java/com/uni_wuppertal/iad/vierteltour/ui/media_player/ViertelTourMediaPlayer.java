222cd508ba5f335a9fef237197a2885a1f7b9f2f
Branislav Bardak | smallfish.eu <bran@smallfish.eu>
2016-07-12 07:20:10 +0200

    Mediaplayer: Create basic Mediaplayer class (doesn't do anything useful yet)

---
+++ app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/media_player/ViertelTourMediaPlayer.java
@@ -0,0 +1,34 @@
+package com.uni_wuppertal.iad.vierteltour.ui.media_player;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class ViertelTourMediaPlayer extends MediaPlayer{
+  private static ViertelTourMediaPlayer ourInstance;
+  /**
+   * Singleton-method
+   *
+   * @return OurPlayer a static singleton of this class
+   */
+  public static ViertelTourMediaPlayer getInstance( Context context){
+    if( ourInstance == null ){
+      ourInstance = new ViertelTourMediaPlayer( context );
+    }
+
+    return ourInstance;
+  }
+
+
+  private Context context;
+
+
+  /** The main constructor.
+   *
+   */
+  private ViertelTourMediaPlayer( Context context){
+    this.context = context;
+
+    //super.;
+  }
+}
+
