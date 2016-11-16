27c1e5077d143f2c0b3a0788622ac8e1a294f3d3
Branislav Bardak | smallfish.eu <bran@smallfish.eu>
2016-11-16 11:58:55 +0100

    XmlParsing(Tour): Can now retrieve one of it's stations by number (defined in XML)

--- app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/map/Tour.java
+++ app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/map/Tour.java
@@ -64,4 +64,29 @@ public class Tour extends TourListData{
 
     return route;
   }
+
+
+  /**
+   * Returns the Station with the same number or an empty Station if the number was not found
+   *
+   * NOTE:
+   * This returns the FIRST station found. There is currently NO error handling if two or more
+   * stations have the same number
+   *
+   * TODO: Implement error handling in case more then one station have the same number, which is only the case if it's already wrong in the source XML
+   *
+   * @param number as defined in the XML
+   * @return the Station
+   */
+  public Station station( int number ){
+    for( Station station : details.stations() ){
+      if( station.number == number ){
+        return station;
+      }
+    }
+
+    // TODO: Use exceptions rather than a simple log message
+    Log.d( DEBUG_TAG, "WARNING: The tour " + name + " does not contain a station with the number " + number );
+    return new Station();
+  }
 }
