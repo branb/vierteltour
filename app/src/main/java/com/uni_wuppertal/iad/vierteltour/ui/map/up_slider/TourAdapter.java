ac9af8e0b9a99e4b4866d06e0ecd30f3baaab176
kevinkol <kevin.kollek@web.de>
2017-05-22 00:59:38 +0200

    Customized Dialog and DrawerIcon

--- app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/map/up_slider/TourAdapter.java
+++ app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/map/up_slider/TourAdapter.java
@@ -9,14 +9,17 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.pixplicity.sharp.Sharp;
@@ -131,6 +134,7 @@ public class TourAdapter extends BaseExpandableListAdapter {
     laden = (ImageView) convertView.findViewById( R.id.laden);
     laden.setTag("laden"+position);
     Sharp.loadResource(context.getResources(), R.raw.laden).into(laden);
+
     downloadtext = (TextView) convertView.findViewById(R.id.downloadtext);
     downloadtext.setTag("text"+position);
     final TextView txtTitle = (TextView) convertView.findViewById( R.id.txt );
@@ -161,7 +165,7 @@ public class TourAdapter extends BaseExpandableListAdapter {
       @Override
       public void onClick(View view) {
         if(context instanceof MapsActivity){
-          createDownloadDialog("Willst du die Tour "+ tours.get(position).name() + " herunterladen?\nHinweis: Verwende dein WLAN", tour.slug(), position);
+          createDownloadDialog("Willst du die Tour >>"+ tours.get(position).name() + "<< herunterladen? Hinweis: Verwende dein WLAN", tour.slug(), position);
 
         }}});
 
@@ -176,6 +180,12 @@ public class TourAdapter extends BaseExpandableListAdapter {
     dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     dialog.show();
     // set values for custom dialog components - text, image and button
+    DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+
+    RelativeLayout dialogWindow = (RelativeLayout) dialog.findViewById(R.id.dialog);
+    dialogWindow.setLayoutParams(new FrameLayout.LayoutParams((int) (displayMetrics.widthPixels*0.85), FrameLayout.LayoutParams.WRAP_CONTENT));
+    TextView downloadtitle = (TextView) dialog.findViewById(R.id.title_dialog);
+    downloadtitle.setText("Laden der Tour");
     TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
     text.setText(txt);
 
@@ -202,89 +212,5 @@ public class TourAdapter extends BaseExpandableListAdapter {
         dialog.dismiss();
       }});
   }
-
-/* if( convertView == null && context instanceof MapsActivity){
-      LayoutInflater mInflater = (LayoutInflater) (context).getSystemService( Activity.LAYOUT_INFLATER_SERVICE );
-      convertView = mInflater.inflate( R.layout.touren_list_single, null );
-    }
-
-
-    // Define the visible elements of a single item inside of our ListView
-    ImageView imgAuthor = (ImageView) convertView.findViewById( R.id.img );
-    geladen = (ImageView) convertView.findViewById( R.id.geladen);
-    //geladen.setTag("ok"+position);
-    Sharp.loadResource(context.getResources(), R.raw.ok).into(geladen);
-    laden = (ImageView) convertView.findViewById( R.id.laden);
-    // laden.setTag("laden"+position);
-    Sharp.loadResource(context.getResources(), R.raw.laden).into(laden);
-    downloadtext = (TextView) convertView.findViewById(R.id.downloadtext);
-    // downloadtext.setTag("text"+position);
-    final TextView txtTitle = (TextView) convertView.findViewById( R.id.txt );
-    TextView txtAuthor = (TextView) convertView.findViewById( R.id.subtxt1 );
-    TextView txtTimeLength = (TextView) convertView.findViewById( R.id.subtxt2 );
-    TextView txtDescription = (TextView) convertView.findViewById( R.id.addinfo );
-    ImageButton btnStart = (ImageButton) convertView.findViewById( R.id.zumstartlist );
-    Sharp.loadResource(context.getResources(), R.raw.zum_start).into(btnStart);
-    View divider = convertView.findViewById( R.id.divider );
-
-    final Tour tour = tours.get( position );
-    convertView.setBackgroundColor( Color.parseColor( tour.color() ) );
-
-    if(sharedPreferences.getBoolean(tour.slug(), false))
-    {geladen.setVisibility(View.VISIBLE);
-      laden.setVisibility(View.GONE);
-      downloadtext.setText("geladen");
-      downloadtext.setVisibility(View.VISIBLE);
-      btnStart.setVisibility(View.VISIBLE);}
-    else{geladen.setVisibility(View.GONE);
-      laden.setVisibility(View.VISIBLE);
-      downloadtext.setVisibility(View.GONE);
-      btnStart.setVisibility(View.INVISIBLE);
-    }
-
-    BitmapFactory.Options options = new BitmapFactory.Options();
-    Bitmap mBitmapInsurance = BitmapFactory.decodeFile(OurStorage.get(context).storagePath()+"/"+OurStorage.get(context).lookForTourFile(((MapsActivity)context).tourlist(),tour.image())+tour.image()+".png" ,options);
-    imgAuthor.setImageBitmap(mBitmapInsurance);
-    //imgAuthor.setImageURI( Uri.fromFile(new File(OurStorage.get(context).storagePath()+"/"+OurStorage.get(context).lookForTourFile(((MapsActivity)context).tourlist(), tour.image())+tour.image()+".png")));
-
-    laden.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        if(context instanceof MapsActivity){
-          createDownloadDialog("Willst du die Tour "+ txtTitle.getText() + " herunterladen?\nHinweis: Verwende dein WLAN", tour.slug(), position);
-
-        }}});
-
-    txtTitle.setText( tour.name() );
-    txtAuthor.setText( tour.author() );
-    txtTimeLength.setText( tour.time() + "/" + tour.length() );
-    txtDescription.setText( tour.description() );
-    convertView.setClickable(false);
-
-    if(singlepage.INSTANCE.selectedTour()==null || !tour.slug().equals(singlepage.INSTANCE.selectedTour().slug())) {
-      txtDescription.setVisibility( View.GONE );
-      btnStart.setVisibility( View.GONE );
-      divider.setVisibility( View.GONE );
-
-    }
-    else if( tour.slug().equals(singlepage.INSTANCE.selectedTour().slug()) ) {
-      txtDescription.setVisibility(View.VISIBLE);
-      if(sharedPreferences.getBoolean(tour.slug(), false))btnStart.setVisibility(View.VISIBLE);
-      divider.setVisibility(View.VISIBLE);
-      convertView.setClickable(true);
-      convertView.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          if(context instanceof MapsActivity) ((MapsActivity)context).resetTour();
-          notifyDataSetChanged();
-          view.setClickable(false);
-
-        }
-      });
-    }
-    view = convertView;
-    ownContainer=parent;
-
-    return convertView;*/
 
 }
