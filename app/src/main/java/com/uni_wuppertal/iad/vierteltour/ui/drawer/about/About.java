2ea498081de715c286e5fa88c7a18a5cb1de4567
kevinkol <kevin.kollek@web.de>
2017-05-23 18:11:56 +0200

    Reworked About function

--- app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/drawer/about/About.java
+++ app/src/main/java/com/uni_wuppertal/iad/vierteltour/ui/drawer/about/About.java
@@ -4,11 +4,19 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.text.SpannableStringBuilder;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.text.Text;
+import com.pixplicity.sharp.Sharp;
 import com.uni_wuppertal.iad.vierteltour.R;
 
 /**
@@ -16,37 +24,95 @@ import com.uni_wuppertal.iad.vierteltour.R;
  * Activity only shows about text.
  */
 
-public class About extends Activity {
-
+public class About extends AppCompatActivity {
+  private ActionBar actionBar;
+  private TextView title;
+  private ImageButton xbtn;
   @Override
   protected void onCreate(
     @Nullable
       Bundle savedInstanceState ){
     super.onCreate( savedInstanceState );
     setContentView( R.layout.about );
+    initActionbar();
+    TextView titleViertelTour = (TextView) findViewById(R.id.title_viertel);
+    titleViertelTour.setText("Was ist ViertelTour?");
 
-    TextView textView = (TextView) findViewById(R.id.abouttext);
+    TextView textViertelTour = (TextView) findViewById(R.id.text_viertel);
 
-    String text = "Die App ViertelTour nimmt Sie mit auf informative Stadtspaziergänge, die von unterschiedlichsten Experten durch eine dichte Ökologie- und Kulturlandschaft im Wuppertaler Innenstadtraum durchgeführt werden.\nEs sind weniger die Sight-Seeing-Highlights, die hier vermittelt werden, sondern eher Orte und Dinge an denen wir täglich vorübergehen, ohne sie jedoch zu bemerken, obwohl sich zu vielen Orten unserer Stadt bemerkenswertes Wissen abrufen lässt. Man muss nur die Richtigen fragen – und das haben etwa 20 Studierende der Fakultät für Kunst und Design im Seminar Design Interaktiver Medien und Tontechnik der Bergischen Universität Wuppertal getan. \n\nLaden Sie sich die Touren am besten bevor Sie losgehen im Wlan Netz herunter. Es werden pro Tour etwa 100–200 MB Daten geladen.\n\nUm die Audio-, Bild, und Video-Daten dann zu den Touren hören und sehen zu können, müssen Sie sich vor Ort befinden!\n\nWir haben ViertelTour bewusst so konzipiert, dass die Spaziergänge tatsächlich nur erlebt werden können, wenn Sie sich genau dort befinden, wo die entsprechende Expertin oder der entsprechende Experte Sie mit auf den Weg nehmen möchte, um Wuppertal vor Ort zu erfahren.\nSie haben mit der App die Möglichkeit sich an die entsprechenden Ort navigieren zu lassen.\nWenn Sie dort angekommen sind, werden Ihnen die Daten jeweils Stationsweise zur Verfügung gestellt.\nDie Touren sind anhand nummerierter Stationen so aufgebaut, dass Sie der Expertin oder dem Experten einfach von Station zu Station folgen können.\nWir wünschen viel Spaß und hoffentlich neue Einsichten und Erfahrungen in einer hochinteressanten Stadt.\n\nProjektgruppe Design Interaktiver Medien, Bergische Universität Wuppertal, Prof. Kristian Wolf\n\n\nDie App ViertelTour entstand im Sommersemester 2015 im Projektseminar Design Interaktiver Medien unter der Leitung von Prof. Kristian Wolf. Fertiggestellt wurde sie im Wintersemester 2016/17\n\nIdee und Grundkonzept: Kristian Wolf, Laura Poplow, Nikolaus Zunner\n\nKonzeption und Screen Design: Muriel Balzer, Tamara Dreke, Franziska Györfi, Ana Ivić\n\nBild und Ton: Studierende des Seminars Licht/Ton, Design Ausdiovisueller Medien unter der Leitung von AOR Till Müller\n\nAufbereitung der Daten: Benjamin Pfingsten, Svenja Krautwurst, Jessica Schmitz\n\nAndroid Programmierung: (SWT-Praktikum: Miodrag Radovanovic, Jannis Güldenpfennig, Michel Verheijen, Thomas Vogt, Branislab Bardak, Kevin Kollek), Branislav Bardak, Kevin Kollek (Bachelor Thesis, Betreuung Dr. Werner Hofschuster)\n\niOS Programmierung: Noah Lotz-Hommann\n\nProjektkoordination: Dr. Christoph Rodatz\n\nWir danken allen ExpertInnen, die mit Ihrem hier preisgegebenen Wissen dazu betragen, dass die Stadt Wuppertal in vielen Facetten, die sich erst auf den zweiten Blick erschließen, erfahrbar wird.\n\nVielen Dank an die Jackstädtstiftung, die das Projekt finanziell unterstützt hat.";
+    textViertelTour.setText(Html.fromHtml( "<br />"
+      + "Die App "+ "<b>" + "ViertelTour" + "</b>" + " nimmt Sie mit auf informative Stadtspaziergänge durch die dichte Ökologie- und Kulturlandschaft der Wuppertaler Innenstadt. Begleitet werden Sie dabei von unterschiedlichsten Expertinnen und Experten."
+      + "<br />" + "<br />" + "Es werden keine Sight-Seeing-Highlights vermittelt, sondern viel mehr Orte und Dinge an denen wir täglich vorüber gehen ohne sie zu bemerken. Zu diesen unscheinbaren Orten lässt sich bemerkenswertes Wissen abrufen − wenn man die richtigen Fragen stellt."
+      +"Und das haben etwa 20 Studierende der Fakultät für Kunst und Design in den " + "<b>" +"Projektseminaren Design Interaktiver Medien"+ "</b>" + " und " + "<b>" + "Tontechnik" + "</b>" + " an der " + "<b>" + "Bergischen Universität Wuppertal" + "</b>" + " getan."
+      + "<br />" + "<br />" + " Um die Audio, Bild, und Video-Daten der jeweiligen Touren hören und sehen zu können," + "<b>" + " müssen Sie sich vor Ort befinden!" + "</b>" + "<br />" + "<br />"
+      + "Am besten" + "<b>" + " laden Sie sich die Touren im WLAN-Netzwerk herunter bevor Sie losgehen." + "</b>" + " Es werden pro Tour etwa 50-200 MB Daten geladen." + "<br />" + "<br />"
+      + "Um Wuppertal vor Ort zu erfahren, haben wir ViertelTour bewusst so konzipiert, dass die Spaziergänge tatsächlich nur erlebt werden können, wenn Sie sich genau dort befinden, wo die entsprechende Expertin oder der entsprechende Experte Sie mit auf den Weg nehmen möchte."
+      +" Sie haben in der App die Möglichkeit sich von Ihrem Standort aus "+"<b>"+"via GPS an die entsprechenden Orte navigieren zu lassen."+"</b>"+"<br />"+"<br />"
+      + "Die Touren sind so aufgebaut, dass Sie der Expertin oder dem Experten einfach von Station zu Station folgen könen. Wenn Sie dort angekommen sind, werden Ihnen die Daten der enstprechenden Station zur Verfügung gestellt."+ "<br />" + "<br />"
+      + "Wir wünschen viel Spaß und hoffentlich neue Einsichten und Erfahrungen in einer hochinteressanten Stadt."+ "<br />" + "<br />" + "Projektgruppe Design Interaktiver Medien, Bergische Universität Wuppertal, Prof. Kristian Wolf"+ "<br />" + "<br />"
+    ));
 
-    final SpannableStringBuilder str = new SpannableStringBuilder(text);      //Texteditierer, um Wörter kursiv zu schreiben
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 8, 19, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);        //ViertelTour
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 565, 592, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);     //Design Interaktiver Medien
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 597, 607, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);     //Tontechnik
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 612, 645, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);     //Bergischen Universität Wuppertal
+    TextView titleMitwirkende = (TextView) findViewById(R.id.title_mitwirkende);
+    titleMitwirkende.setText("Wer hat mitgewirkt?");
 
-    str.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 780, 900, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
-    str.setSpan(new RelativeSizeSpan(1.2f), 780, 900, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+    TextView textMitwirkende = (TextView) findViewById(R.id.text_mitwirkende);
 
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 910, 922, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);        //ViertelTour
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 1613, 1710, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);        //Projektgruppe
+    textMitwirkende.setText(Html.fromHtml( "<br />"
+      + "Die App ViertelTour entstand im Sommersemester 2015 im Projektseminar Design Interaktiver Medien unter der Leitung von Prof. Kristian Wolf. Fertiggestellt wurde sie im Wintersemester 2016/2017."+"<br />" +"<br />"
+      +"<b>"+"Idee und Grundkonzept"+"</b>"+"<br />" +"Kristian Wolf, Laura Poplow, Nikolaus Zunner"+"<br />"+"<br />"+"<b>"+"Konzeption und Screen Design"+"</b>"+"<br />"+"Muriel Balzer, Tamara Dreke, Franziska Györfi, Ana Ivić"+"<br />"+"<br />"
+      +"<b>"+"Bild und Ton"+"</b>"+"<br />"+"Studierende des Seminars Licht/Ton, Design Ausdiovisueller Medien unter der Leitung von AOR Till Müller"+"<br />"+"<br />"+"<b>"+"Aufbereitung der Daten"+"</b>"+"<br />"+"Benjamin Pfingsten, Svenja Krautwurst, Jessica Schmitz"
+      +"<br />"+"<br />"+"<b>" + "Android Programmierung" + "</b>" +"<br />" +"Softwaretechnik-Praktikum"+"<br />"+"Miodrag Radovanovic, Jannis Güldenpfennig, Michel Verheijen, Thomas Vogt, Branislav Bardak, Kevin Kollek"+"<br />"+"<br />"
+      +"Bachelor-Thesis, Betreuung Dr. Werner Hofschuster"+"<br />" + "Kevin Kollek"+"<br />"+"<br />"
+      +"<b>"+"iOS Programmierung"+"</b>"+"<br />"+"Noah Lotz-Hommann"+"<br />"+"<br />"+"<b>"+"Projektkoordination"+"</b>"+"<br />"+"Dr. Christoph Rodatz"+"<br />"+"<br />"));
 
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 1719, 1730, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);        //ViertelTour
-    str.setSpan(new StyleSpan(Typeface.ITALIC), 1780, 1807, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);     //Design Interaktiver Medien
 
-    textView.setText(str);
+    TextView titleDanksagung = (TextView) findViewById(R.id.title_danksagung);
+    titleDanksagung.setText("Danksagung");
 
+    TextView textDanksagung = (TextView) findViewById(R.id.text_danksagung);
+    textDanksagung.setText(Html.fromHtml( "<br />" + "Wir danken " + "<b>" + "allen Expertinnen und Experten" + "</b>" + ", die mit ihrem hier preisgegebenen Wissen dazu beitragen, die Stadt Wupertal in vielen, erst auf den zweiten Blick erkennbaren Facetten erfahrbar zu machen."
+      +"<br />"+"<br />"+ "Vielen Dank an die "+"<b>" + "Jackstädtstiftung" + "</b>" + ", die das Projekt finanziell unterstützt hat."
+    ));
+
+   /*
+
+
+
+    Wir danken allen Expertinnen und Experten, die mit ihrem hier preisgegebenen Wissen dazu beitragen, die Stadt Wupertal in vielen, erst auf den zweiten Blick erkennbaren Facetten erfahrbar zu machen.
+
+      Vielen Dank an die Jackstädtstiftung, die das Projekt finanziell unterstützt hat."
+*/
+  }
+  public void initActionbar()
+  {actionBar = getSupportActionBar();
+
+    actionBar.setDisplayShowHomeEnabled( false );
+    actionBar.setDisplayShowTitleEnabled( false );
+    actionBar.setDisplayHomeAsUpEnabled( false );
+    actionBar.setHomeButtonEnabled( false );
+    actionBar.setDisplayShowCustomEnabled( true );     //Deaktiviert alle Buttons und setzt CostumActionBar
+    View view = getLayoutInflater().inflate( R.layout.toolbar, null );
+    android.support.v7.app.ActionBar.LayoutParams layoutParams = new android.support.v7.app.ActionBar.LayoutParams( android.support.v7.app.ActionBar.LayoutParams.MATCH_PARENT, android.support.v7.app.ActionBar.LayoutParams.MATCH_PARENT );
+    actionBar.setCustomView( view, layoutParams );
+    Toolbar parent = (Toolbar) view.getParent();
+    parent.setContentInsetsAbsolute( 0, 0 );           //Vermeidet Fehler, dass CostumActionBar zu schmal wird
+    actionBar.setElevation( 0 );
+
+
+    xbtn = (ImageButton) findViewById(R.id.btn_x);      //ActionBar Button: Right
+    title = (TextView) findViewById(R.id.toolbar_title);  //ActionBar Title
+    title.setText("About");
+    xbtn.setOnClickListener( new View.OnClickListener(){
+      @Override
+      public void onClick( View v ){
+        onBackPressed();
+      }
+    });
+    Sharp.loadResource(getResources(), R.raw.beenden_dunkel).into(xbtn);
+
+    title.setVisibility(View.VISIBLE);
+    xbtn.setVisibility(View.VISIBLE);
+    title.setTypeface(Typeface.MONOSPACE);
 
   }
-
 }
